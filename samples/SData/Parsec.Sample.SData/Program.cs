cea9c488f94a746f515b4c36e16c4cfd0289a911
Matias <matigramirez@gmail.com>
2021-10-25 01:32:41 -0300

    Initial sample projects

---
+++ samples/SData/Parsec.Sample.SData/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Parsec.Shaiya;
+
+namespace Parsec.Sample.SData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var npcQuest = new NpcQuest("NpcQuest.SData");
+
+            npcQuest.SaveDecrypted("NpcQuest.decrypted.SData");
+            npcQuest.SaveEncrypted("NpcQuest.encrypted.SData");
+
+            npcQuest.Read();
+
+            // foreach (var merchant in npcQuest.Merchants)
+            // {
+            //     Console.WriteLine(merchant.Name);
+            // }
+
+            foreach (var gatekeeper in npcQuest.GateKeepers)
+            {
+                Console.WriteLine(gatekeeper.Name);
+            }
+        }
+    }
+}
---
+++ samples/svmap/Parsec.Sample.Svmap/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Parsec.Sample.Svmap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var svmap = new Shaiya.SVMAP.Svmap("0.svmap");
+            svmap.Read();
+
+            Console.WriteLine($"MapSize: {svmap.MapSize}");
+            Console.WriteLine($"Ladder Count: {svmap.LadderCount}");
+            Console.WriteLine($"Monster Area Count: {svmap.MonsterAreaCount}");
+            Console.WriteLine($"Npc Count: {svmap.NpcCount}");
+            Console.WriteLine($"Portal Count: {svmap.PortalCount}");
+            Console.WriteLine($"Spawn Count: {svmap.SpawnCount}");
+            Console.WriteLine($"Named Area Count: {svmap.NamedAreaCount}");
+        }
+    }
+}
