51e9b4207c6acc7b9f090e771439da105bffb01b
Matias <matigramirez@gmail.com>
2021-10-27 15:19:07 -0300

    updated svmap sample

--- samples/svmap/Parsec.Sample.Svmap/Program.cs
+++ samples/svmap/Parsec.Sample.Svmap/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Parsec.Shaiya.SVMAP;
 
 namespace Parsec.Sample.SVMAP
@@ -7,16 +8,26 @@ namespace Parsec.Sample.SVMAP
     {
         static void Main(string[] args)
         {
-            var svmap = new Svmap("0.svmap");
-            svmap.Read();
+            var svmap0 = new Svmap("0.svmap");
+            svmap0.Read();
 
-            Console.WriteLine($"MapSize: {svmap.MapSize}");
-            Console.WriteLine($"Ladder Count: {svmap.LadderCount}");
-            Console.WriteLine($"Monster Area Count: {svmap.MonsterAreaCount}");
-            Console.WriteLine($"Npc Count: {svmap.NpcCount}");
-            Console.WriteLine($"Portal Count: {svmap.PortalCount}");
-            Console.WriteLine($"Spawn Count: {svmap.SpawnCount}");
-            Console.WriteLine($"Named Area Count: {svmap.NamedAreaCount}");
+            var svmap2 = new Svmap("2.svmap");
+            svmap2.Read();
+
+            Console.WriteLine($"File: {svmap2.FileName}");
+            Console.WriteLine($"MapSize: {svmap2.MapSize}");
+            Console.WriteLine($"Ladder Count: {svmap2.Ladders.Count}");
+            Console.WriteLine($"Monster Area Count: {svmap2.MonsterAreas.Count}");
+            Console.WriteLine($"Npc Count: {svmap2.Npcs.Count}");
+            Console.WriteLine($"Portal Count: {svmap2.Portals.Count}");
+            Console.WriteLine($"Spawn Count: {svmap2.Spawns.Count}");
+            Console.WriteLine($"Named Area Count: {svmap2.NamedAreas.Count}");
+
+            svmap2.Export($"{svmap2.FileNameWithoutExtension}.json", new List<string>
+            {
+                nameof(svmap2.MapHeights),
+                nameof(svmap2.MapSize)
+            });
         }
     }
 }
