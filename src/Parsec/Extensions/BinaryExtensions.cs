a70ad7c2d948dea66d8d84202e7b937282c03b86
Matias <matigramirez@gmail.com>
2021-11-30 13:46:48 -0300

    Add BinaryExtensions

---
+++ src/Parsec/Extensions/BinaryExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Parsec.Extensions
+{
+    public static class BinaryExtensions
+    {
+        public static byte[] GetBytes(this short value) => BitConverter.GetBytes(value);
+
+        public static byte[] GetBytes(this ushort value) => BitConverter.GetBytes(value);
+
+        public static byte[] GetBytes(this int value) => BitConverter.GetBytes(value);
+
+        public static byte[] GetBytes(this uint value) => BitConverter.GetBytes(value);
+
+        public static byte[] GetASCIILengthPrefixedBytes(this string str, bool includeStringTerminator = true)
+        {
+            var buffer = new List<byte>();
+
+            var finalLength = includeStringTerminator ? str.Length + 1 : str.Length;
+            var finalStr = includeStringTerminator ? str + '\0' : str;
+
+            buffer.AddRange(BitConverter.GetBytes(finalLength));
+            buffer.AddRange(Encoding.ASCII.GetBytes(finalStr));
+
+            return buffer.ToArray();
+        }
+    }
+}
