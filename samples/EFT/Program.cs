dbdbbee2c16ec09b8ab43aaf44bf829fa4b69d7f
Matias <matigramirez@gmail.com>
2021-12-04 21:38:33 -0300

    Moved samples to main solution and improved solution organization

---
+++ samples/EFT/Program.cs
@@ -0,0 +1,30 @@
+﻿using Parsec.Helpers;
+using Parsec.Shaiya.EFT;
+
+namespace Parsec.Samples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExportJsonFromEFT("Monster.EFT", "Monster.json");
+            CreateEFTFromJson("Monster.json", "Monster.Created.EFT");
+        }
+
+        static void ExportJsonFromEFT(string eftPath, string jsonPath)
+        {
+            var eft = new EFT(eftPath);
+            eft.Read();
+            eft.ExportJson(jsonPath);
+        }
+
+        static void CreateEFTFromJson(string jsonPath, string eftPath)
+        {
+            // Importing the created json file
+            var eftFromJson = Deserializer.ReadFromJson<EFT>(jsonPath);
+
+            // Export the loaded json file
+            eftFromJson.Write(eftPath);
+        }
+    }
+}
---
+++ samples/seff/Program.cs
@@ -0,0 +1,31 @@
+﻿using Parsec.Helpers;
+using Parsec.Shaiya.SEFF;
+
+namespace Parsec.Samples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExportJsonFromSeff("weapon.seff", "weapon.seff.json");
+
+            CreateSeffFromJson("weapon.seff.json", "weapon_createdfromjson.seff");
+        }
+
+        static void ExportJsonFromSeff(string seffPath, string jsonPath)
+        {
+            var seff = new Seff(seffPath);
+            seff.Read();
+            seff.ExportJson(jsonPath);
+        }
+
+        static  void CreateSeffFromJson(string jsonPath, string seffPath)
+        {
+            // Importing the created json file
+            var seffFromJson = Deserializer.ReadFromJson<Seff>(jsonPath);
+
+            // Export the loaded json file
+            seffFromJson.Write(seffPath);
+        }
+    }
+}
