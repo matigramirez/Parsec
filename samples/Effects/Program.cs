c0848550d411c6d20a4d40581e48efd2850143a7
Matias <matigramirez@gmail.com>
2021-12-04 23:55:43 -0300

    Merge related sample projects

---
+++ samples/Effects/Program.cs
@@ -0,0 +1,27 @@
+﻿using Parsec.Shaiya.EFT;
+using Parsec.Shaiya.SEFF;
+
+namespace Parsec.Samples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region EFT
+
+            var eft = new EFT("Monster.EFT");
+            eft.Read();
+            eft.ExportJson($"{eft.FileName}.json");
+
+            #endregion
+
+            #region seff
+
+            var seff = new Seff("weapon.seff");
+            seff.Read();
+            seff.ExportJson($"{seff.FileName}.json");
+
+            #endregion
+        }
+    }
+}
