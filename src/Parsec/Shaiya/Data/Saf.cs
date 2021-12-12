37534a2447b4c8858b19fa4f252cd8e5a9827533
Matias <matigramirez@gmail.com>
2021-12-12 00:21:00 -0300

    SAF: Removed redundant code and fixed extracting error

--- src/Parsec/Shaiya/Data/Saf.cs
+++ src/Parsec/Shaiya/Data/Saf.cs
@@ -1,5 +1,4 @@
 ﻿using System.IO;
-using Parsec.Extensions;
 using Parsec.Helpers;
 
 namespace Parsec.Shaiya.DATA
@@ -20,14 +19,10 @@ namespace Parsec.Shaiya.DATA
         /// Extracts all the content of the saf file into a directory
         /// </summary>
         /// <param name="extractionPath">Path where files should be saved</param>
-        public void Extract(string extractionPath)
+        public void ExtractAll(string extractionPath)
         {
             Directory.CreateDirectory(extractionPath);
-
-            // Create binary reader
-            using var safReader = new BinaryReader(File.OpenRead(_sah.SafPath));
-
-            ExtractFolder(_sah.RootFolder, extractionPath);
+            Extract(_sah.RootFolder, extractionPath);
         }
 
         /// <summary>
@@ -35,57 +30,24 @@ namespace Parsec.Shaiya.DATA
         /// </summary>
         /// <param name="folder">Folder to extract</param>
         /// <param name="extractionDirectory">Directory where folder should be extracted</param>
-        private void ExtractFolder(SahFolder folder, string extractionDirectory)
+        public void Extract(SahFolder folder, string extractionDirectory)
         {
+            var extractionPath = Path.Combine(extractionDirectory, folder.Name);
+            Directory.CreateDirectory(extractionPath);
+
             // Extract files
             foreach (var file in folder.Files)
-            {
-                ExtractFile(file, extractionDirectory);
-            }
+                Extract(file, extractionPath);
 
             // Extract subfolders
             foreach (var subfolder in folder.Subfolders)
-            {
-                var path = Path.Combine(extractionDirectory, subfolder.Name);
-
-                Directory.CreateDirectory(path);
-
-                ExtractFolder(subfolder, path);
-            }
-        }
-
-        /// <summary>
-        /// Extracts a shaiya file from the saf
-        /// </summary>
-        /// <param name="file">File to extract</param>
-        /// <param name="extractionDirectory">Directory where file should be saved</param>
-        private void ExtractFile(SahFile file, string extractionDirectory)
-        {
-            // Create binary reader
-            using var safReader = new BinaryReader(File.OpenRead(_sah.SafPath));
-
-            // Skip files with invalid characters
-            if (file.Name.HasInvalidCharacters())
-                return;
-
-            var path = Path.Combine(extractionDirectory, file.Name);
-
-            using var fileWriter = new BinaryWriter(File.OpenWrite(path));
-
-            // Set offset to file's starting offset
-            safReader.BaseStream.Seek(file.Offset, SeekOrigin.Begin);
-
-            // Read file bytes (length bytes)
-            var bytesToWrite = safReader.ReadBytes(file.Length);
-
-            // Write bytes to file
-            fileWriter.Write(bytesToWrite);
+                Extract(subfolder, extractionPath);
         }
 
         /// <summary>
         /// Extracts a single file into a directory
         /// </summary>
-        /// <param name="file">The <see cref="SahFile"/> instance to extract</param>
+        /// <param name="file">The <see cref="SahFile" /> instance to extract</param>
         /// <param name="extractionDirectory">The directory where the file should be saved</param>
         public void Extract(SahFile file, string extractionDirectory)
         {
@@ -102,12 +64,10 @@ namespace Parsec.Shaiya.DATA
         {
             // Create binary reader
             using var safReader = new BinaryReader(File.OpenRead(_sah.SafPath));
-
             safReader.BaseStream.Seek(offset, SeekOrigin.Begin);
 
             // Read bytes
             byte[] bytes = safReader.ReadBytes(length);
-
             return bytes;
         }
     }
