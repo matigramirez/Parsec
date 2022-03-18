47e52b80731134d8fa608cb4af70069a3436f424
Matias Ramirez <matigramirez@gmail.com>
2022-03-18 14:29:16 -0300

    DBMonsterText: Add support for file format

---
+++ src/Parsec/Shaiya/Monster/DBMonsterTextRecord.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using Parsec.Extensions;
+using Parsec.Readers;
+using Parsec.Shaiya.SData;
+
+namespace Parsec.Shaiya.Monster
+{
+    public class DBMonsterTextRecord : IBinarySDataRecord
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+
+        public void Read(SBinaryReader binaryReader, params object[] options)
+        {
+            Id = binaryReader.Read<long>();
+            Name = binaryReader.ReadString();
+        }
+
+        public byte[] GetBytes(params object[] options)
+        {
+            var buffer = new List<byte>();
+            buffer.AddRange(Id.GetBytes());
+            buffer.AddRange(Name.GetBytes());
+            return buffer.ToArray();
+        }
+    }
+}
