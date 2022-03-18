c1547e1f4e311c8f0b51b4df7d25ed1a051fea98
Matias Ramirez <matigramirez@gmail.com>
2022-03-18 15:59:27 -0300

    DBItemSellText: Add support for file format

---
+++ src/Parsec/Shaiya/Cash/DBItemSellTextRecord.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Parsec.Extensions;
+using Parsec.Readers;
+using Parsec.Shaiya.SData;
+
+namespace Parsec.Shaiya.Cash
+{
+    public class DBItemSellTextRecord : IBinarySDataRecord
+    {
+        public long Id { get; set; }
+        public string ProductName { get; set; }
+        public string Text { get; set; }
+
+        public void Read(SBinaryReader binaryReader, params object[] options)
+        {
+            Id = binaryReader.Read<long>();
+            ProductName = binaryReader.ReadString();
+            Text = binaryReader.ReadString();
+        }
+
+        public byte[] GetBytes(params object[] options)
+        {
+            var buffer = new List<byte>();
+            buffer.AddRange(Id.GetBytes());
+            buffer.AddRange(ProductName.GetLengthPrefixedBytes());
+            buffer.AddRange(Text.GetLengthPrefixedBytes());
+            return buffer.ToArray();
+        }
+    }
+}
