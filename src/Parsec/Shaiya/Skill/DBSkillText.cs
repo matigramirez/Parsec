3ddd2ba02ca1d771804227ed460a3ae1b1ce9db7
Matias Ramirez <matigramirez@gmail.com>
2022-03-18 14:51:25 -0300

    DBSkillText: add support for file format

---
+++ src/Parsec/Shaiya/Skill/DBSkillText.cs
@@ -0,0 +1,8 @@
+﻿using Parsec.Shaiya.SData;
+
+namespace Parsec.Shaiya.Skill
+{
+    public class DBSkillText : BinarySData<DBSkillTextRecord>
+    {
+    }
+}
---
+++ src/Parsec/Shaiya/Skill/DBSkillTextRecord.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using Parsec.Extensions;
+using Parsec.Readers;
+using Parsec.Shaiya.SData;
+
+namespace Parsec.Shaiya.Skill
+{
+    public class DBSkillTextRecord : IBinarySDataRecord
+    {
+        public long Id { get; set; }
+        public long SkillLevel { get; set; }
+        public string Name { get; set; }
+        public string Text { get; set; }
+
+        public void Read(SBinaryReader binaryReader, params object[] options)
+        {
+            Id = binaryReader.Read<long>();
+            SkillLevel = binaryReader.Read<long>();
+            Name = binaryReader.ReadString();
+            Text = binaryReader.ReadString();
+        }
+
+        public byte[] GetBytes(params object[] options)
+        {
+            var buffer = new List<byte>();
+            buffer.AddRange(Id.GetBytes());
+            buffer.AddRange(SkillLevel.GetBytes());
+            buffer.AddRange(Name.GetLengthPrefixedBytes());
+            buffer.AddRange(Text.GetLengthPrefixedBytes());
+            return buffer.ToArray();
+        }
+    }
+}
