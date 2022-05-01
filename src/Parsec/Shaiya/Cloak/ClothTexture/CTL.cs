fc4b6c65f110651f47755a83d64a49df7c53c59d
Bowie <96175709+l3owie@users.noreply.github.com>
2022-05-01 16:32:41 -0400

    Cloak: Add CTL and Emblem dat support

---
+++ src/Parsec/Shaiya/Cloak/ClothTexture/CTL.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+using Parsec.Common;
+using Parsec.Extensions;
+using Parsec.Shaiya.Core;
+
+namespace Parsec.Shaiya.CTL
+{
+    public class CTL : FileBase, IJsonReadable
+    {
+        public int Count { get; set; }
+
+        public List<Texture> Textures { get; } = new();
+
+        [JsonIgnore]
+        public override string Extension => "CTL";
+
+        [JsonConstructor]
+        public CTL()
+        {
+        }
+
+        public override void Read(params object[] options)
+        {
+            Count = _binaryReader.Read<int>();
+
+            for (int i = 0; i < Count; i++)
+            {
+                var texture = new Texture(_binaryReader);
+                Textures.Add(texture);
+            }
+        }
+
+        public override byte[] GetBytes(params object[] options)
+        {
+            var buffer = new List<byte>();
+            buffer.AddRange(Textures.GetBytes());
+            return buffer.ToArray();
+        }
+    }
+}
