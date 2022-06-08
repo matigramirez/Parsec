77631b245eb86e4e9416d87260579e7fe6eb98d2
Matias <matigramirez@gmail.com>
2022-06-08 17:19:22 -0300

    Add FixedLengthListAttribute and SuffixedStringAttribute

---
+++ src/Parsec/Attributes/FixedLengthListAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Parsec.Attributes;
+
+[AttributeUsage(AttributeTargets.Property)]
+public class FixedLengthListAttribute : Attribute
+{
+    public Type ItemType { get; set; }
+    public int Length { get; set; }
+
+    public FixedLengthListAttribute(Type itemType, int length)
+    {
+        ItemType = itemType;
+        Length = length;
+    }
+}
---
+++ src/Parsec/Attributes/SuffixedStringAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Parsec.Attributes;
+
+[AttributeUsage(AttributeTargets.Property)]
+public class SuffixedStringAttribute : Attribute
+{
+    public string Suffix { get; set; }
+
+    public SuffixedStringAttribute(string suffix)
+    {
+        Suffix = suffix;
+    }
+}
