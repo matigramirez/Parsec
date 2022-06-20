42b868f89f39e25a6ad889ce5e23a2719d6892a5
Matias <matigramirez@gmail.com>
2022-06-20 16:54:49 -0300

    KillStatus: use attribute based api

--- src/Parsec/Shaiya/KillStatus/KillStatusBonus.cs
+++ src/Parsec/Shaiya/KillStatus/KillStatusBonus.cs
@@ -1,33 +1,18 @@
-﻿using Newtonsoft.Json;
-using Parsec.Extensions;
-using Parsec.Readers;
-using Parsec.Shaiya.Core;
+﻿using Parsec.Attributes;
 
 namespace Parsec.Shaiya.KillStatus;
 
-public class KillStatusBonus : IBinary
+public class KillStatusBonus
 {
+    /// <summary>
+    /// The type of bonus
+    /// </summary>
+    [ShaiyaProperty]
     public KillStatusBonusType Type { get; set; }
+
+    /// <summary>
+    /// The value of the bonus
+    /// </summary>
+    [ShaiyaProperty]
     public short Value { get; set; }
-
-    [JsonConstructor]
-    public KillStatusBonus()
-    {
-    }
-
-    public KillStatusBonus(SBinaryReader binaryReader)
-    {
-        Type = (KillStatusBonusType)binaryReader.Read<byte>();
-        Value = binaryReader.Read<short>();
-    }
-
-    public IEnumerable<byte> GetBytes(params object[] options)
-    {
-        var buffer = new List<byte>();
-
-        buffer.Add((byte)Type);
-        buffer.AddRange(Value.GetBytes());
-
-        return buffer;
-    }
 }
