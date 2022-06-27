22f37ffeacb623105c72c500933b632366887ea9
Matias <matigramirez@gmail.com>
2022-06-27 14:56:17 -0300

    DBDualLayerClothesData: Add support for file format

---
+++ src/Parsec/Shaiya/DualLayerClothes/DBDualLayerClothesData.cs
@@ -0,0 +1,7 @@
+﻿using Parsec.Shaiya.SData;
+
+namespace Parsec.Shaiya.DualLayerClothes;
+
+public class DBDualLayerClothesData : BinarySData<DBDualLayerClothesDataRecord>
+{
+}
---
+++ src/Parsec/Shaiya/DualLayerClothes/DBDualLayerClothesDataRecord.cs
@@ -0,0 +1,28 @@
+﻿using Parsec.Attributes;
+using Parsec.Shaiya.SData;
+
+namespace Parsec.Shaiya.DualLayerClothes;
+
+public class DBDualLayerClothesDataRecord : IBinarySDataRecord
+{
+    [ShaiyaProperty]
+    public long Id { get; set; }
+
+    [ShaiyaProperty]
+    public long Top { get; set; }
+
+    [ShaiyaProperty]
+    public long Hand { get; set; }
+
+    [ShaiyaProperty]
+    public long Bottom { get; set; }
+
+    [ShaiyaProperty]
+    public long Shoe { get; set; }
+
+    [ShaiyaProperty]
+    public long Empty { get; set; }
+
+    [ShaiyaProperty]
+    public long Helmet { get; set; }
+}
