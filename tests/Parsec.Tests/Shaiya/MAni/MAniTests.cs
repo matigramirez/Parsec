f0a5e897faec7e1826f232a13aa38cd005806565
Matias Ramirez <matigramirez@gmail.com>
2022-10-27 23:58:05 -0300

    Tests: add MAni, MLT and Seff

---
+++ tests/Parsec.Tests/Shaiya/MAni/MAniTests.cs
@@ -0,0 +1,38 @@
+﻿using Xunit;
+
+namespace Parsec.Tests.Shaiya.MAni;
+
+public class MAniTests
+{
+    [Theory]
+    [InlineData("A8_DUN_2F_statue_ANI.MAni")]
+    [InlineData("Boat_B2.MAni")]
+    [InlineData("halloween_pumkin02.MAni")]
+    [InlineData("Login_floatrock01a.MAni")]
+    [InlineData("login_rotate02.MAni")]
+    [InlineData("raputa.MAni")]
+    [InlineData("stoneimage.MAni")]
+    [InlineData("turn_rocks02.MAni")]
+    [InlineData("wing.MAni")]
+    [InlineData("Wing_small.MAni")]
+    [InlineData("World_01.MAni")]
+    public void MAniMultipleReadWriteTest(string fileName)
+    {
+        string filePath = $"Shaiya/MAni/{fileName}";
+        string jsonPath = $"Shaiya/MAni/{fileName}.json";
+        string newObjPath = $"Shaiya/MAni/new_{fileName}";
+
+        var mani = Reader.ReadFromFile<Parsec.Shaiya.MAni.MAni>(filePath);
+        mani.ExportJson(jsonPath);
+        var maniFromJson = Reader.ReadFromJson<Parsec.Shaiya.MAni.MAni>(jsonPath);
+
+        // Check bytes
+        Assert.Equal(mani.GetBytes(), maniFromJson.GetBytes());
+
+        maniFromJson.Write(newObjPath);
+        var newMani = Reader.ReadFromFile<Parsec.Shaiya.MAni.MAni>(newObjPath);
+
+        // Check bytes
+        Assert.Equal(mani.GetBytes(), newMani.GetBytes());
+    }
+}
---
+++ tests/Parsec.Tests/Shaiya/Seff/SeffTests.cs
@@ -0,0 +1,32 @@
+﻿using Xunit;
+
+namespace Parsec.Tests.Shaiya.Seff;
+
+public class SeffTests
+{
+    [Theory]
+    [InlineData("2009_03_18.Seff")]
+    [InlineData("EventEffect.Seff")]
+    [InlineData("Login_cloud.Seff")]
+    [InlineData("weapon.Seff")]
+    [InlineData("Weather.Seff")]
+    public void SeffMultipleReadWriteTest(string fileName)
+    {
+        string filePath = $"Shaiya/Seff/{fileName}";
+        string jsonPath = $"Shaiya/Seff/{fileName}.json";
+        string newObjPath = $"Shaiya/Seff/new_{fileName}";
+
+        var seff = Reader.ReadFromFile<Parsec.Shaiya.Seff.Seff>(filePath);
+        seff.ExportJson(jsonPath);
+        var seffFromJson = Reader.ReadFromJson<Parsec.Shaiya.Seff.Seff>(jsonPath);
+
+        // Check bytes
+        Assert.Equal(seff.GetBytes(), seffFromJson.GetBytes());
+
+        seffFromJson.Write(newObjPath);
+        var newSeff = Reader.ReadFromFile<Parsec.Shaiya.Seff.Seff>(newObjPath);
+
+        // Check bytes
+        Assert.Equal(seff.GetBytes(), newSeff.GetBytes());
+    }
+}
