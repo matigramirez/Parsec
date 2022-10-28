5fe19d4d228341105bd5ec7b2de6cd0c44555353
Matias Ramirez <matigramirez@gmail.com>
2022-10-28 01:27:20 -0300

    MON: Fix writing

--- tests/Parsec.Tests/Shaiya/MON/MonTests.cs
+++ tests/Parsec.Tests/Shaiya/MON/MonTests.cs
@@ -15,14 +15,20 @@ public class MonTests
     public void MonMultipleReadWriteTest(string fileName)
     {
         string filePath = $"Shaiya/MON/{fileName}";
+        string outputPath = $"Shaiya/MON/output_{fileName}";
         string jsonPath = $"Shaiya/MON/{fileName}.json";
         string newObjPath = $"Shaiya/MON/new_{fileName}";
 
         var mon = Reader.ReadFromFile<Parsec.Shaiya.MON.MON>(filePath);
+
+        mon.Write(outputPath);
         mon.ExportJson(jsonPath);
+
+        var outputMon = Reader.ReadFromFile<Parsec.Shaiya.MON.MON>(outputPath);
         var monFromJson = Reader.ReadFromJson<Parsec.Shaiya.MON.MON>(jsonPath);
 
         // Check bytes
+        Assert.Equal(mon.GetBytes(), outputMon.GetBytes());
         Assert.Equal(mon.GetBytes(), monFromJson.GetBytes());
 
         monFromJson.Write(newObjPath);
