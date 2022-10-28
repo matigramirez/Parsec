aeec00571998d13a13666680ae8eb654072c3c46
Matias Ramirez <matigramirez@gmail.com>
2022-10-28 17:11:25 -0300

    Tests: update cash test

--- tests/Parsec.Tests/Shaiya/Cash/CashTests.cs
+++ tests/Parsec.Tests/Shaiya/Cash/CashTests.cs
@@ -5,7 +5,7 @@ namespace Parsec.Tests.Shaiya.Cash;
 public class CashTests
 {
     [Fact]
-    public void CashCsvTest()
+    public void CashTest()
     {
         const string filePath = "Shaiya/Cash/Cash.SData";
         const string outputPath = "Shaiya/Cash/Cash_new.SData";
@@ -14,5 +14,6 @@ public class CashTests
         cash.Write(outputPath);
         var newCash = Reader.ReadFromFile<Parsec.Shaiya.Cash.Cash>(outputPath);
         Assert.Equal(cash.GetBytes(), newCash.GetBytes());
+        Assert.Equal(FileHash.Checksum(filePath), FileHash.Checksum(outputPath));
     }
 }
