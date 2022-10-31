b6d95eae8440f4fe2a70d28777956e70a7e4032d
Matias Ramirez <matigramirez@gmail.com>
2022-10-31 19:21:48 -0300

    Tests: Add skill and NpcSkill

---
+++ tests/Parsec.Tests/Shaiya/NpcSkill/NpcSkillTests.cs
@@ -0,0 +1,55 @@
+﻿using System.Linq;
+using Parsec.Shaiya.NpcSkill;
+
+namespace Parsec.Tests.Shaiya.NpcSkill;
+
+public class NpcSkillTests
+{
+    [Fact]
+    public void DbSkillTest()
+    {
+        const string filePath = "Shaiya/NpcSkill/DBNpcSkillData.SData";
+        const string outputPath = "Shaiya/NpcSkill/output_DBNpcSkillData.SData";
+        const string jsonPath = "Shaiya/NpcSkill/DBNpcSkillData.SData.json";
+        const string csvPath = "Shaiya/NpcSkill/DBNpcSkillData.SData.csv";
+
+        var npcSkill = Reader.ReadFromFile<DBNpcSkillData>(filePath);
+        npcSkill.Write(outputPath);
+        npcSkill.ExportJson(jsonPath);
+        npcSkill.ExportCsv(csvPath);
+
+        var outputNpcSkill = Reader.ReadFromFile<DBNpcSkillData>(outputPath);
+        var jsonNpcSkill = Reader.ReadFromJson<DBNpcSkillData>(jsonPath);
+        var csvNpcSkill = DBNpcSkillData.ReadFromCsv<DBNpcSkillData>(csvPath);
+
+        var expected = npcSkill.GetBytes().ToList();
+        Assert.Equal(expected, outputNpcSkill.GetBytes());
+        Assert.Equal(expected, jsonNpcSkill.GetBytes());
+        // For the csv check, skip the 128-byte header, which gets lost in the process
+        Assert.Equal(expected.Skip(128), csvNpcSkill.GetBytes().Skip(128));
+    }
+
+    [Fact]
+    public void DbSkillTextTest()
+    {
+        const string filePath = "Shaiya/NpcSkill/DBNpcSkillText_USA.SData";
+        const string outputPath = "Shaiya/NpcSkill/output_DBNpcSkillText_USA.SData";
+        const string jsonPath = "Shaiya/NpcSkill/DBNpcSkillText_USA.SData.json";
+        const string csvPath = "Shaiya/NpcSkill/DBNpcSkillText_USA.SData.csv";
+
+        var npcSkillText = Reader.ReadFromFile<DBNpcSkillText>(filePath);
+        npcSkillText.Write(outputPath);
+        npcSkillText.ExportJson(jsonPath);
+        npcSkillText.ExportCsv(csvPath);
+
+        var outputNpcSkillText = Reader.ReadFromFile<DBNpcSkillText>(outputPath);
+        var jsonNpcSkillText = Reader.ReadFromJson<DBNpcSkillText>(jsonPath);
+        var csvNpcSkillText = DBNpcSkillText.ReadFromCsv<DBNpcSkillText>(csvPath);
+
+        var expected = npcSkillText.GetBytes().ToList();
+        Assert.Equal(expected, outputNpcSkillText.GetBytes());
+        Assert.Equal(expected, jsonNpcSkillText.GetBytes());
+        // For the csv check, skip the 128-byte header, which gets lost in the process
+        Assert.Equal(expected.Skip(128), csvNpcSkillText.GetBytes().Skip(128));
+    }
+}
---
+++ tests/Parsec.Tests/Shaiya/Skill/SkillTests.cs
@@ -0,0 +1,55 @@
+﻿using System.Linq;
+using Parsec.Shaiya.Skill;
+
+namespace Parsec.Tests.Shaiya.Skill;
+
+public class SkillTests
+{
+    [Fact]
+    public void DbSkillTest()
+    {
+        const string filePath = "Shaiya/Skill/DBSkillData.SData";
+        const string outputPath = "Shaiya/Skill/output_DBSkillData.SData";
+        const string jsonPath = "Shaiya/Skill/DBSkillData.SData.json";
+        const string csvPath = "Shaiya/Skill/DBSkillData.SData.csv";
+
+        var dbSkill = Reader.ReadFromFile<DBSkillData>(filePath);
+        dbSkill.Write(outputPath);
+        dbSkill.ExportJson(jsonPath);
+        dbSkill.ExportCsv(csvPath);
+
+        var outputDbSkill = Reader.ReadFromFile<DBSkillData>(outputPath);
+        var jsonDbSkill = Reader.ReadFromJson<DBSkillData>(jsonPath);
+        var csvSkill = DBSkillData.ReadFromCsv<DBSkillData>(csvPath);
+
+        var expected = dbSkill.GetBytes().ToList();
+        Assert.Equal(expected, outputDbSkill.GetBytes());
+        Assert.Equal(expected, jsonDbSkill.GetBytes());
+        // For the csv check, skip the 128-byte header, which gets lost in the process
+        Assert.Equal(expected.Skip(128), csvSkill.GetBytes().Skip(128));
+    }
+
+    [Fact]
+    public void DbSkillTextTest()
+    {
+        const string filePath = "Shaiya/Skill/DBSkillText_USA.SData";
+        const string outputPath = "Shaiya/Skill/output_DBSkillText_USA.SData";
+        const string jsonPath = "Shaiya/Skill/DBSkillText_USA.SData.json";
+        const string csvPath = "Shaiya/Skill/DBSkillText_USA.SData.csv";
+
+        var skillText = Reader.ReadFromFile<DBSkillText>(filePath);
+        skillText.Write(outputPath);
+        skillText.ExportJson(jsonPath);
+        skillText.ExportCsv(csvPath);
+
+        var outputSkillText = Reader.ReadFromFile<DBSkillText>(outputPath);
+        var jsonSkillText = Reader.ReadFromJson<DBSkillText>(jsonPath);
+        var csvSkillText = DBSkillText.ReadFromCsv<DBSkillText>(csvPath);
+
+        var expected = skillText.GetBytes().ToList();
+        Assert.Equal(expected, outputSkillText.GetBytes());
+        Assert.Equal(expected, jsonSkillText.GetBytes());
+        // For the csv check, skip the 128-byte header, which gets lost in the process
+        Assert.Equal(expected.Skip(128), csvSkillText.GetBytes().Skip(128));
+    }
+}
