19baa89d30d61df3845ab2525140594e6c427358
Matias Ramirez <matigramirez@gmail.com>
2023-03-19 00:07:59 -0300

    tests: add GuildHouse

---
+++ tests/Parsec.Tests/Shaiya/GuildHouse/GuildHouseTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+
+namespace Parsec.Tests.Shaiya.GuildHouse;
+
+public class GuildHouseTests
+{
+    [Fact]
+    public void GuildHouseTest()
+    {
+        const string filePath = "Shaiya/GuildHouse/GuildHouse.SData";
+        const string outputPath = "Shaiya/GuildHouse/GuildHouse.output.SData";
+        const string jsonPath = "Shaiya/GuildHouse/GuildHouse.SData.json";
+        var npcQuest = Reader.ReadFromFile<Parsec.Shaiya.GuildHouse.GuildHouse>(filePath);
+        npcQuest.Write(outputPath);
+        npcQuest.ExportJson(jsonPath);
+
+        var outputNpcQuest = Reader.ReadFromFile<Parsec.Shaiya.GuildHouse.GuildHouse>(outputPath);
+        var jsonNpcQuest = Reader.ReadFromJsonFile<Parsec.Shaiya.GuildHouse.GuildHouse>(jsonPath);
+
+        var expected = npcQuest.GetBytes().ToList();
+        Assert.Equal(expected, outputNpcQuest.GetBytes());
+        Assert.Equal(expected, jsonNpcQuest.GetBytes());
+    }
+}
