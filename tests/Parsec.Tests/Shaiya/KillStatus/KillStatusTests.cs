a41e5b2477461a443238850d7850c5b6bb2a4f5e
Kurt Raburn <136869066+sdev23@users.noreply.github.com>
2023-07-12 14:28:12 -0400

    Tests: add KillStatus coverage

---
+++ tests/Parsec.Tests/Shaiya/KillStatus/KillStatusTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+
+namespace Parsec.Tests.Shaiya.KillStatus;
+
+public class KillStatusTests
+{
+    [Fact]
+    public void KillStatusTest()
+    {
+        const string filePath = "Shaiya/KillStatus/KillStatus.SData";
+        const string outputPath = "Shaiya/KillStatus/KillStatus.output.SData";
+        const string jsonPath = "Shaiya/KillStatus/KillStatus.json";
+
+        var killStatus = Reader.ReadFromFile<Parsec.Shaiya.KillStatus.KillStatus>(filePath);
+        killStatus.Write(outputPath);
+        killStatus.ExportJson(jsonPath);
+
+        var killStatusData = Reader.ReadFromFile<Parsec.Shaiya.KillStatus.KillStatus>(outputPath);
+        var killStatusJson = Reader.ReadFromJsonFile<Parsec.Shaiya.KillStatus.KillStatus>(jsonPath);
+
+        var expected = killStatus.GetBytes().ToList();
+        Assert.Equal(expected, killStatusData.GetBytes());
+        Assert.Equal(expected, killStatusJson.GetBytes());
+    }
+}
