460cff3110e7a95bad6c863aa420cdfd95e5ba1b
Matias Ramirez <matigramirez@gmail.com>
2023-08-03 23:39:12 -0300

    Wtr: use String256

--- src/Parsec/Shaiya/Wtr/Wtr.cs
+++ src/Parsec/Shaiya/Wtr/Wtr.cs
@@ -1,5 +1,6 @@
 ﻿using Parsec.Common;
 using Parsec.Extensions;
+using Parsec.Shaiya.Common;
 using Parsec.Shaiya.Core;
 
 namespace Parsec.Shaiya.Wtr;
@@ -7,29 +8,25 @@ namespace Parsec.Shaiya.Wtr;
 public sealed class Wtr : FileBase
 {
     public float Unknown1 { get; set; }
-    public int Unknown2 { get; set; }
+    public uint Unknown2 { get; set; }
     public int Unknown3 { get; set; }
 
     /// <summary>
     /// List of texture file names with a fixed lenght of 256
     /// </summary>
-    public List<string> Textures { get; } = new();
+    public List<String256> Textures { get; } = new();
 
     public override string Extension => "wtr";
 
     public override void Read(params object[] options)
     {
         Unknown1 = _binaryReader.Read<float>();
-        Unknown2 = _binaryReader.Read<int>();
+        Unknown2 = _binaryReader.Read<uint>();
         Unknown3 = _binaryReader.Read<int>();
 
         int textureCount = _binaryReader.Read<int>();
         for (int i = 0; i < textureCount; i++)
-        {
-            // Read 256-byte strings and trim the null characters
-            string texture = _binaryReader.ReadString(256).Trim('\0');
-            Textures.Add(texture);
-        }
+            Textures.Add(new String256(_binaryReader));
     }
 
     public override IEnumerable<byte> GetBytes(Episode episode = Episode.Unknown)
@@ -38,17 +35,7 @@ public sealed class Wtr : FileBase
         buffer.AddRange(Unknown1.GetBytes());
         buffer.AddRange(Unknown2.GetBytes());
         buffer.AddRange(Unknown3.GetBytes());
-        buffer.AddRange(Textures.Count.GetBytes());
-
-        foreach (string texture in Textures)
-        {
-            buffer.AddRange(texture.GetBytes());
-
-            // Add extra null characters if needed if needed
-            if (texture.Length < 256)
-                buffer.AddRange(new byte[256 - texture.Length]);
-        }
-
+        buffer.AddRange(Textures.GetBytes());
         return buffer;
     }
 }
