422b3912aaa2f9b0745481f92bcd6504cae4a76f
matias <matigramirez@gmail.com>
2023-10-08 17:04:17 -0300

    test(ItemData): add test

--- tests/Parsec.Tests/Shaiya/Item/ItemTests.cs
+++ tests/Parsec.Tests/Shaiya/Item/ItemTests.cs
@@ -1,4 +1,6 @@
-﻿using Parsec.Common;
+﻿using System.Linq;
+using Parsec.Common;
+using Parsec.Shaiya.Item;
 
 namespace Parsec.Tests.Shaiya.Item;
 
@@ -40,5 +42,53 @@ public class ItemTests
         item.WriteCsv(csvPath, encoding);
         var itemFromCsv = Parsec.Shaiya.Item.Item.ReadFromCsv(csvPath, Episode.EP6, encoding);
         Assert.Equal(item.GetBytes(Episode.EP6), itemFromCsv.GetBytes(Episode.EP6));
+    }
+
+    [Fact]
+    public void DbItemTest()
+    {
+        const string filePath = "Shaiya/Item/DBItemData.SData";
+        const string outputPath = "Shaiya/Item/output_DBItemData.SData";
+        const string jsonPath = "Shaiya/Item/DBItemData.SData.json";
+        const string csvPath = "Shaiya/Item/DBItemData.SData.csv";
+
+        var dbItem = ParsecReader.ReadFromFile<DBItemData>(filePath);
+        dbItem.Write(outputPath);
+        dbItem.WriteJson(jsonPath);
+        dbItem.WriteCsv(csvPath);
+
+        var outputDbItem = ParsecReader.ReadFromFile<DBItemData>(outputPath);
+        var jsonDbItem = ParsecReader.ReadFromJsonFile<DBItemData>(jsonPath);
+        var csvItem = DBItemData.ReadFromCsv<DBItemData>(csvPath);
+
+        var expected = dbItem.GetBytes().ToList();
+        Assert.Equal(expected, outputDbItem.GetBytes());
+        Assert.Equal(expected, jsonDbItem.GetBytes());
+        // For the csv check, skip the 128-byte header, which gets lost in the process
+        Assert.Equal(expected.Skip(128), csvItem.GetBytes().Skip(128));
+    }
+
+    [Fact]
+    public void DbItemTextTest()
+    {
+        const string filePath = "Shaiya/Item/DBItemText_USA.SData";
+        const string outputPath = "Shaiya/Item/output_DBItemText_USA.SData";
+        const string jsonPath = "Shaiya/Item/DBItemText_USA.SData.json";
+        const string csvPath = "Shaiya/Item/DBItemText_USA.SData.csv";
+
+        var itemText = ParsecReader.ReadFromFile<DBItemText>(filePath);
+        itemText.Write(outputPath);
+        itemText.WriteJson(jsonPath);
+        itemText.WriteCsv(csvPath);
+
+        var outputItemText = ParsecReader.ReadFromFile<DBItemText>(outputPath);
+        var jsonItemText = ParsecReader.ReadFromJsonFile<DBItemText>(jsonPath);
+        var csvItemText = DBItemText.ReadFromCsv<DBItemText>(csvPath);
+
+        var expected = itemText.GetBytes().ToList();
+        Assert.Equal(expected, outputItemText.GetBytes());
+        Assert.Equal(expected, jsonItemText.GetBytes());
+        // For the csv check, skip the 128-byte header, which gets lost in the process
+        Assert.Equal(expected.Skip(128), csvItemText.GetBytes().Skip(128));
     }
 }
