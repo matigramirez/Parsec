0406290e0cc8800c32f55efd16c5f4e284dbcb95
Matias Ramirez <matigramirez@gmail.com>
2024-04-19 16:52:52 -0300

    add System.Text.Json source generator definitions

---
+++ src/Parsec.Json/ParsecJsonSerializerContext.cs
@@ -0,0 +1,89 @@
+﻿using System.Text.Json.Serialization;
+using Parsec.Shaiya._3dc;
+using Parsec.Shaiya._3de;
+using Parsec.Shaiya._3do;
+using Parsec.Shaiya.Alt;
+using Parsec.Shaiya.Ani;
+using Parsec.Shaiya.Cash;
+using Parsec.Shaiya.Cloak.ClothTexture;
+using Parsec.Shaiya.Cloak.Emblem;
+using Parsec.Shaiya.Data;
+using Parsec.Shaiya.Dg;
+using Parsec.Shaiya.DualLayerClothes;
+using Parsec.Shaiya.Eft;
+using Parsec.Shaiya.GuildHouse;
+using Parsec.Shaiya.Item;
+using Parsec.Shaiya.Itm;
+using Parsec.Shaiya.KillStatus;
+using Parsec.Shaiya.Mani;
+using Parsec.Shaiya.Mlt;
+using Parsec.Shaiya.Mlx;
+using Parsec.Shaiya.Mon;
+using Parsec.Shaiya.Monster;
+using Parsec.Shaiya.NpcQuest;
+using Parsec.Shaiya.NpcSkill;
+using Parsec.Shaiya.Seff;
+using Parsec.Shaiya.SetItem;
+using Parsec.Shaiya.Skill;
+using Parsec.Shaiya.Smod;
+using Parsec.Shaiya.Svmap;
+using Parsec.Shaiya.TransformModel;
+using Parsec.Shaiya.Vani;
+using Parsec.Shaiya.Wld;
+using Parsec.Shaiya.Wtr;
+using Parsec.Shaiya.Zon;
+
+namespace Parsec.Json;
+
+[JsonSourceGenerationOptions(WriteIndented = true)]
+[JsonSerializable(typeof(_3dc))]
+[JsonSerializable(typeof(_3dc))]
+[JsonSerializable(typeof(_3de))]
+[JsonSerializable(typeof(_3do))]
+[JsonSerializable(typeof(Alt))]
+[JsonSerializable(typeof(Ani))]
+[JsonSerializable(typeof(Cash))]
+[JsonSerializable(typeof(Cash))]
+[JsonSerializable(typeof(Cash))]
+[JsonSerializable(typeof(Ctl))]
+[JsonSerializable(typeof(EmblemDat))]
+[JsonSerializable(typeof(Sah))]
+[JsonSerializable(typeof(Dg))]
+[JsonSerializable(typeof(DualLayerClothes))]
+[JsonSerializable(typeof(DBDualLayerClothesData))]
+[JsonSerializable(typeof(Eft))]
+[JsonSerializable(typeof(GuildHouse))]
+[JsonSerializable(typeof(Item))]
+[JsonSerializable(typeof(DBItemData))]
+[JsonSerializable(typeof(DBItemText))]
+[JsonSerializable(typeof(Itm))]
+[JsonSerializable(typeof(KillStatus))]
+[JsonSerializable(typeof(Mani))]
+[JsonSerializable(typeof(Mlt))]
+[JsonSerializable(typeof(Mlx))]
+[JsonSerializable(typeof(Mon))]
+[JsonSerializable(typeof(Monster))]
+[JsonSerializable(typeof(DBMonsterData))]
+[JsonSerializable(typeof(DBMonsterText))]
+[JsonSerializable(typeof(NpcQuest))]
+[JsonSerializable(typeof(NpcQuestTrans))]
+[JsonSerializable(typeof(DBNpcSkillData))]
+[JsonSerializable(typeof(DBNpcSkillText))]
+[JsonSerializable(typeof(Seff))]
+[JsonSerializable(typeof(SetItem))]
+[JsonSerializable(typeof(DBSetItemData))]
+[JsonSerializable(typeof(DBSetItemText))]
+[JsonSerializable(typeof(Skill))]
+[JsonSerializable(typeof(DBSkillData))]
+[JsonSerializable(typeof(DBSkillText))]
+[JsonSerializable(typeof(Smod))]
+[JsonSerializable(typeof(Svmap))]
+[JsonSerializable(typeof(DBTransformModelData))]
+[JsonSerializable(typeof(DBTransformWeaponModelData))]
+[JsonSerializable(typeof(Vani))]
+[JsonSerializable(typeof(Wld))]
+[JsonSerializable(typeof(Wtr))]
+[JsonSerializable(typeof(Zon))]
+public partial class ParsecJsonSerializerContext : JsonSerializerContext
+{
+}
---
+++ src/Parsec.Json/ParsecJsonWriter.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json;
+
+namespace Parsec.Json;
+
+public static class ParsecJsonWriter
+{
+    public static void WriteJson<T>(this T instance, string path) where T : class
+    {
+        var json = JsonSerializer.Serialize(instance, typeof(T), ParsecJsonSerializerContext.Default);
+        File.WriteAllText(path, json);
+    }
+}
