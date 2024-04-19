4f6255810e58ef331fabcb1cdcf1c69845165522
Matias Ramirez <matigramirez@gmail.com>
2024-04-19 16:52:31 -0300

    remove tests project

--- tests/Parsec.Tests/TestEncodings.cs
+++
@@ -1,9 +0,0 @@
-using System.Text;
-
-namespace Parsec.Tests;
-
-public static class TestEncodings
-{
-    public static readonly Encoding Encoding1251 = CodePagesEncodingProvider.Instance.GetEncoding(1251);
-    public static readonly Encoding Encoding1252 = CodePagesEncodingProvider.Instance.GetEncoding(1252);
-}
