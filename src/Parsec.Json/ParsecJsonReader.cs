3a6cba4423eab6d1464deb892a4ba69595bbf7a7
Matias Ramirez <matigramirez@gmail.com>
2024-04-19 17:45:12 -0300

    feat(Json): add json source generator

---
+++ src/Parsec.Json/ParsecJsonReader.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json;
+
+namespace Parsec.Json;
+
+public static class ParsecJsonReader
+{
+    public static T? FromJsonFile<T>(string path) where T : class
+    {
+        var options = new JsonSerializerOptions
+        {
+            TypeInfoResolver = ParsecJsonSerializerContext.Default
+        };
+
+        return JsonSerializer.Deserialize<T>(File.ReadAllText(path), options);
+    }
+}
